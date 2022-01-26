6c8bbc22cf7ffd554bdb3932a5a5defd90cf3b57
gsvartsund <gustavsv@uia.no>
2022-01-26 15:14:08 +0100

    Modul 1

---
+++ brun.go
@@ -0,0 +1,24 @@
+package myquote
+import (
+	"fmt"
+	"rsc.io/quote"
+
+)
+
+func Glassquote(){
+	fmt.Println(quote.Glass())
+
+}
+
+func Goquote(){
+	fmt.Println(quote.Go())
+}
+
+func Helloquote(){
+	fmt.Println(quote.Hello())
+
+}
+
+func Optquote(){
+	fmt.Println(quote.Opt())
+}
